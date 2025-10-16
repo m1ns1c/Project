a3d9cfd28623e60a63f36ea1deb1e234d6ad2d48
m1ns1c <jms62436423@gmail.com>
2025-10-16 16:43:01 +0900

    Remove turret function and related input handling

--- 터렛.py
+++ 터렛.py
@@ -1,16 +1,1 @@
-import math
 
-def turret(a, b, c, d, e, f):
-    t = math.sqrt((d - a) ** 2 + (e - b) ** 2)
-    if t == 0 and c == f:
-        return -1
-    if t > c + f or t < abs(c - f):
-        return 0
-    if t == c + f or t == abs(c - f):
-        return 1
-    return 2
-
-n = int(input()) 
-for i in range(n):
-    a, b, c, d, e, f = map(int, input().split())
-    print(turret(a, b, c, d, e, f))
